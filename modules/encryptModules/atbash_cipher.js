46f7052c94a3800fb8189b13b65e760752f2f431
Ol_Kuts <kuts.ol.vas@gmail.com>
2021-11-03 07:43:17 +0200

    feat: atbash_cipher

--- modules/encryptModules/atbash_cipher.js
+++ modules/encryptModules/atbash_cipher.js
@@ -1,5 +1,17 @@
-const atbash_cipher = (data, key) => {
-  return data;
+const atbash_cipher = (data) => {
+  console.log(data);
+  const result = data.split('').reduce((out, leter) => {
+    if (/[a-zA-Z]/.test(leter)) {
+      const char = leter.charCodeAt();
+      let start = char - 65 > 26 ? 97 : 65;
+      return out + String.fromCharCode(2 * start + 25 - char);
+    } else {
+      return out + leter
+    }
+  }, '');
+
+  console.log(result);
+  return result;
 }
 
 module.exports = atbash_cipher;
